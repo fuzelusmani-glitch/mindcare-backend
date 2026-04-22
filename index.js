8cfa678ddb58e6e5876d8924cd28c1164b9b899c
fuzelusmani-glitch <fuzelusmai@gmail.com>
2026-04-22 13:25:33 +0530

    fix cors final

--- index.js
+++ index.js
@@ -15,21 +15,13 @@ const app = express();
  
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://mindcare-frontend-puce.vercel.app"
+  "https://mindcare-frontend-puce.vercel.app",
+  "https://mindcare-frontend-git-main-fuzelusmani-glitchs-projects.vercel.app"
 ];
 app.use(cors(
   {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-    return callback(new Error("Not allowed by CORS"));
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-})
-);
+  origin:"*",
+  }));
  
 app.use(express.json());
 app.use("/api",authRoutes);
